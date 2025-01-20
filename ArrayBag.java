7b714ae1240a587f40b84f240bedb426bace58eb
Angelina-riveros <angelinariv24@gmail.com>
2025-01-20 14:52:41 -0500

    Update ArrayBag.java

--- ArrayBag.java
+++ ArrayBag.java
@@ -229,8 +229,8 @@ public final class ArrayBag<T> implements BagInterface<T>
 
 	public BagInterface<T> difference(BagInterface<T> anotherBag){
 		ArrayBag<T> differentItems = new ArrayBag<T>();
-		for(T outputs: anotherBag.toArray()){
-			if(!this.contains(outputs)){
+		for(T outputs: this.toArray()){
+			if(!anotherBag.contains(outputs)){
 					differentItems.add(outputs);
 			}
 		}
