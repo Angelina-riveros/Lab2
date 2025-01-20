5b9ca98f378b446d5c4ad11400318a9ca337f2d9
Angelina-riveros <angelinariv24@gmail.com>
2025-01-20 15:45:13 -0500

    Update ArrayBag.java

--- ArrayBag.java
+++ ArrayBag.java
@@ -7,7 +7,7 @@ public final class ArrayBag<T> implements BagInterface<T>
 {
 	private final T[] bag; 
 	private int numberOfEntries;
-	 private boolean integrityOK = false;
+	private boolean integrityOK = false;
 	private static final int DEFAULT_CAPACITY = 25;
 	private static final int MAX_CAPACITY = 10000;
 
@@ -227,13 +227,14 @@ public final class ArrayBag<T> implements BagInterface<T>
 		return sameItems;
 	}
 
-	@Override
 	public BagInterface<T> difference(BagInterface<T> anotherBag){
 		ArrayBag<T> differentItems = new ArrayBag<T>();
-		for(T outputs: this.toArray()){
-			if(!anotherBag.contains(outputs)){
-				differentItems.add(outputs);
-			}
+		for (T outputs: this.toArray()){
+			differentItems.add(item);
+		}
+		
+		for (T outputs: anotherBag.toArray()){
+			differentItems.remove(item);
 		}
 		return differentItems;
 	}
